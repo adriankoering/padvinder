96358b378a3bf6a03f8618aab7bb15d09baba426
Adrian Köring <30828061+adriankoering@users.noreply.github.com>
2018-01-21 13:37:13 +0100

    Barebones restructuring of repository

---
+++ padvinder/__main__.py
@@ -0,0 +1,9 @@
+"""
+The main routine setting up a scene and tying all components together.
+"""
+
+from padvinder.ray import Ray
+
+if __name__ == "__main__":
+    print("Hello Padvinder")
+    print("Default Ray - {}".format(Ray()))
