bb438e1f69582e46fc415e054d25973e437b0062
Adrian Köring <30828061+adriankoering@users.noreply.github.com>
2018-01-27 09:57:29 +0100

    Add testcase for check_input

--- padvinder/test/test_ray.py
+++ padvinder/test/test_ray.py
@@ -5,8 +5,37 @@ from padvinder.ray import Ray
 
 class TestRay(unittest.TestCase):
     def test_default_construction(self):
+        """
+        Test if the ray is constructed with The
+        expected default parameters.
+        """
         r = Ray()
         nt.assert_almost_equal(r.p, (0, 0, 0))
         nt.assert_almost_equal(r.d, (1, 0, 0))
         nt.assert_almost_equal(r.p, r._p)
         nt.assert_almost_equal(r.d, r._d)
+
+    def test_input(self):
+        """
+        Test if the input values are checked
+        correctly.
+        Values that do not validate the checks
+        are omitted because they are covered by
+        the remaining tests.
+        """
+        with self.assertRaises(ValueError):
+            Ray(np.array((np.nan, 0, 0)))
+        with self.assertRaises(ValueError):
+            Ray(np.array((np.inf, 0, 0)))
+        with self.assertRaises(ValueError):
+            Ray(np.array((np.inf, 0, 0)))
+        with self.assertRaises(ValueError):
+            Ray(np.array(d=(np.nan, 0, 0)))
+        with self.assertRaises(ValueError):
+            Ray(np.array(d=(np.inf, 0, 0)))
+        with self.assertRaises(ValueError):
+            Ray(np.array(d=(-np.inf, 0, 0)))
+        Ray(np.array((np.nan, 0, 0)), check_input=False)
+        Ray(np.array((np.inf, 0, 0)), check_input=False)
+        Ray(np.array(d=(np.nan, 0, 0)), check_input=False)
+        Ray(np.array(d=(np.inf, 0, 0)), check_input=False)
