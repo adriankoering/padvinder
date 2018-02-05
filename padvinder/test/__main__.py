8dd85300050eb8e0cc76061f064cca86ee365793
Adrian Köring <30828061+adriankoering@users.noreply.github.com>
2018-02-05 22:07:21 +0100

    Add testcases to runner

--- padvinder/test/__main__.py
+++ padvinder/test/__main__.py
@@ -3,8 +3,11 @@
 """
 import unittest
 
-from padvinder.test.test_ray import TestRay
-from padvinder.test.test_util import TestNormalize
-
+from padvinder.test.test_util     import TestNormalize
+from padvinder.test.test_util     import TestCheckFinite
+from padvinder.test.test_ray      import TestRay
+from padvinder.test.test_material import TestMaterial
+from padvinder.test.test_material import TestEmission
+from padvinder.test.test_material import TestLambert
 
 unittest.main()
