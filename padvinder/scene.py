b7c45d083c5fca9a342e56cb7b50ef7a9d3eaf41
Adrian Köring <30828061+adriankoering@users.noreply.github.com>
2018-02-15 08:31:20 +0100

    Add scene and tests

---
+++ padvinder/scene.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+class Scene(object):
+    def __init__(self, *renderable):
+        """
+        A Scene abstracts away intersection tests with a collection of
+        renderable objects (eg. geometry).
+
+        A renderable has to support renderable.intersect(Ray).
+        """
+        self._renderable = list(renderable)
+
+    def __iter__(self):
+        return iter(self._renderable)
+
+    def intersect(self, ray):
+        """
+        Performs intersection tests with every renderable in the scene.
+
+        Arguments
+            ray : Ray
+                the light ray to trace through the scene
+                has to support ray.position and ray.direction
+
+        Returns
+            (number, renderable) where
+            number is in [0, np.inf] - the distance along the ray to the
+                object surface and
+            renderable is an object previously passed into the scene that is
+                intersected by the ray with the shortest distance.
+
+        """
+        out_dist = np.inf
+        out_obj = None
+
+        for obj in self:
+            dist = obj.intersect(ray)
+            if dist < out_dist:
+                out_dist = dist
+                out_obj  = obj
+
+        return out_dist, out_obj
---
+++ padvinder/test/test_scene.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+import unittest
+import numpy.testing as nt
+
+from padvinder.ray      import Ray
+from padvinder.geometry import Sphere
+from padvinder.geometry import Plane
+from padvinder.scene    import Scene
+
+class TestScene(unittest.TestCase):
+    def setUp(self):
+        self.r1 = Ray()
+        self.s1 = Sphere(position=(-2, 0, 0)) # intersect at np.inf
+        self.s2 = Sphere(position=( 2, 0, 0)) # intersect at 1
+        self.p1 = Plane(position=(3, 0, 0))   # intersect at np.inf
+        self.p2 = Plane(position=(.5, 0, 0), normal=(1, 0, 0)) # at 0.5
+
+        self.scn = Scene(self.s1, self.s2, self.p1, self.p2)
+
+    def test_interator(self):
+        for i, obj in enumerate(self.scn):
+            self.assertTrue(self.scn._renderable[i] is obj)
+
+        scn_iter = iter(self.scn)
+        self.assertTrue(next(scn_iter) is self.s1)
+        self.assertTrue(next(scn_iter) is self.s2)
+        self.assertTrue(next(scn_iter) is self.p1)
+        self.assertTrue(next(scn_iter) is self.p2)
+
+    def test_intersection(self):
+        (d, o) = self.scn.intersect(self.r1)
+        nt.assert_almost_equal(d, 0.5)
+        self.assertTrue(o is self.p2)
+
+        r2 = Ray(direction=(0, 1, 0)) # does not intersect scene
+        (d, o) = self.scn.intersect(r2)
+        nt.assert_almost_equal(d, np.inf)
+        self.assertTrue(o is None)
