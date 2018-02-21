a299f70c7c1165b9f27c55bd691a23a25af1f02f
Adrian Köring <30828061+adriankoering@users.noreply.github.com>
2018-02-21 22:00:16 +0100

    Setup simple scene

--- padvinder/__main__.py
+++ padvinder/__main__.py
@@ -5,16 +5,31 @@ The main entry point, tying everything together and producing a rendered image.
 
 .. moduleauthor:: Adrian Köring
 """
+from padvinder.material import Emission
+from padvinder.material import Lambert
 
-from padvinder.ray import Ray
+from padvinder.geometry import Plane
+from padvinder.geometry import Sphere
+
+from padvinder.scene    import Scene
 
 def main():
     """
     This example sets up a scene and kicks off a render which is shown upon
     completion.
     """
-    print("Hello Padvinder")
-    print("Default Ray - {}".format(Ray()))
+    light = Emission()
+    green = Lambert((0.2, 1.0, 0.2))
+    red   = Lambert((1.0, 0.2, 0.2))
+    p1 = Plane(light, (0, 10, 0))
+    p2 = Plane(green, (0, -5, 0))
+
+    s  = Sphere((0, 0, -5))
+
+    scn = Scene(p1, p2, s)
+
+
+
 
 
 if __name__ == "__main__":
